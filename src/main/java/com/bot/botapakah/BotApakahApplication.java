17fb3ff1df64cb1451ed95d1bca1afc7972ca6d6
athallahannafis <athallahannafis@gmail.com>
2019-04-17 07:12:05 +0700

    Test run bot

---
+++ src/main/java/com/bot/botapakah/BotApakahApplication.java
@@ -0,0 +1,61 @@
+package com.bot.botapakah;
+
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+
+@SpringBootApplication
+public class BotApakahApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BotApakahApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotApakahApplication.class, args);
+    }
+
+
+    @EventMapping
+    public void handleTextEvent(MessageEvent<TextMessageContent> messageEvent) {
+        String msg = messageEvent.getMessage().getText().toLowerCase();
+        String[] msgSplit = msg.split(" ");
+        if (msgSplit[0].equals("apakah")) {
+            String answer = getRandomAnswer();
+            String replyToken = messageEvent.getReplyToken();
+            replyChatWithRandomAnswer(replyToken, answer);
+        }
+    }
+
+    public String getRandomAnswer() {
+        Random random = new Random();
+        int num = random.nextInt();
+        if (num % 2 == 0) {
+            return "YA";
+        } else {
+            return "TIDAK";
+        }
+    }
+
+    public void replyChatWithRandomAnswer(String replyToken, String answer) {
+        TextMessage answerInMessage = new TextMessage(answer);
+        try {
+            lineMessagingClient.replyMessage(new ReplyMessage(replyToken, answerInMessage).get());
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Ada error saat ingin membalas chat");
+        }
+    }
+
+
+
+}
