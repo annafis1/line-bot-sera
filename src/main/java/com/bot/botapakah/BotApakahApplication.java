29dd845028d0e0f3ed0f4876bb9ec7e67c49a466
athallahannafis <athallahannafis@gmail.com>
2019-04-18 13:42:47 +0700

    Test

--- src/main/java/com/bot/botapakah/BotApakahApplication.java
+++ src/main/java/com/bot/botapakah/BotApakahApplication.java
@@ -6,6 +6,7 @@ import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,11 +16,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
+
 @SpringBootApplication
+@LineMessageHandler
 public class BotApakahApplication extends SpringBootServletInitializer {
 
     @Autowired
-    public LineMessagingClient lineMessagingClient;
+    private LineMessagingClient lineMessagingClient;
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
@@ -30,37 +33,37 @@ public class BotApakahApplication extends SpringBootServletInitializer {
         SpringApplication.run(BotApakahApplication.class, args);
     }
 
-
     @EventMapping
-    public void handleTextEvent(MessageEvent<TextMessageContent> messageEvent) {
-        String msg = messageEvent.getMessage().getText().toLowerCase();
-        String[] msgSplit = msg.split(" ");
-        if (msgSplit[0].equals("test")) {
-            String answer = getRandomAnswer();
+    public void handleTextEvent(MessageEvent<TextMessageContent> messageEvent){
+        String pesan = messageEvent.getMessage().getText().toLowerCase();
+        String[] pesanSplit = pesan.split(" ");
+        if(pesanSplit[0].equals("apakah")){
+            String jawaban = getRandomJawaban();
             String replyToken = messageEvent.getReplyToken();
-            replyChatWithRandomAnswer(replyToken, answer);
+            balasChatDenganRandomJawaban(replyToken, jawaban);
         }
     }
 
-    public String getRandomAnswer() {
-        Random random = new Random();
-        int num = random.nextInt();
-        if (num % 2 == 0) {
-            return "YA";
-        } else {
-            return "TIDAK";
+    private String getRandomJawaban(){
+        String jawaban = "";
+        int random = new Random().nextInt();
+        if(random%2==0){
+            jawaban = "Ya";
+        } else{
+            jawaban = "Nggak";
         }
+        return jawaban;
     }
 
-    public void replyChatWithRandomAnswer(String replyToken, String answer) {
-        TextMessage answerInMessage = new TextMessage(answer);
+    private void balasChatDenganRandomJawaban(String replyToken, String jawaban){
+        TextMessage jawabanDalamBentukTextMessage = new TextMessage(jawaban);
         try {
-            lineMessagingClient.replyMessage(new ReplyMessage(replyToken, answerInMessage)).get();
+            lineMessagingClient
+                    .replyMessage(new ReplyMessage(replyToken, jawabanDalamBentukTextMessage))
+                    .get();
         } catch (InterruptedException | ExecutionException e) {
             System.out.println("Ada error saat ingin membalas chat");
         }
     }
-
-
 
 }
